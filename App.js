e62b78e5cd5411b580a9f056958670e84eac4b45
Pratik Gaikwad <pratikgaikwad551@gmail.com>
2021-06-21 20:44:44 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,32 @@
+const express=require('express');
+const server = express();
+server.use(express.json());
+
+function validation(firstname,lastname,age){
+    if(firstname.length>50 || lastname.length>50){
+        return false;
+    }
+
+    if(typeof age!==Number){
+        return false;
+    }
+
+    return true;
+}
+
+server.post('/consumer',(request,response)=>{
+    const {firstname,lastname,email,age}=request.body;
+    if(validation(firstname,lastname,age)){
+        response.status(200).json({
+            message:"Validation Successful"  
+        });
+        
+    }
+    else{
+        response.status(400).json({
+            message:"error"       
+        })
+    }
+})
+
+server.listen(3000,()=> console.log("Server is Live"));
