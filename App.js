1666390a53c513091e9d428baba0388370cf3607
Pratik Gaikwad <pratikgaikwad551@gmail.com>
2021-06-21 21:30:39 +0530

    added middleware to route

--- App.js
+++ App.js
@@ -3,31 +3,28 @@ const server = express();
 server.use(express.json());
 server.use(bodyParser.urlencoded());
 
-function validation(firstname,lastname,age){
+const validation(req,res,next){
+    const {firstname,lastname,email,age}=request.body;
     if(firstname.length>50 || lastname.length>50){
-        return false;
+        res.status(400).json({
+            message:"firstame or lastname is big"
+        })
     }
 
     if(typeof age!==Number){
-        return false;
-    }
-
-    return true;
-}
-
-server.post('/consumer',(request,response)=>{
-    const {firstname,lastname,email,age}=request.body;
-    if(validation(firstname,lastname,age)){
-        response.status(200).json({
-            message:"Validation Successful"  
-        });
-        
-    }
-    else{
-        response.status(400).json({
-            message:"error"       
+        res.status(400).json({
+            message:"age must be number"
         })
     }
+
+    next();
+}
+
+server.post('/consumer',validation,(request,response)=>{
+    response.status(200).json({
+        message:"validation successful"
+    })
+    
 })
 
 server.listen(3000,()=> console.log("Server is Live"));
